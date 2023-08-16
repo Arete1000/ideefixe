4201855542dd44bbfa52f05805e8e4212feadbec
Sofia Echegaray <sofia.sings@gmail.com>
2023-08-16 16:46:24 -0700

    TWO QUARTER CIRCLES. NEXT STOP: WORLD DOMINATION!

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import "./App.css";
 import { useState } from "react";
 
-function App() {
+function App() 
+{
   const [pink, setPink] = useState({ r: 255, g: 205, b: 232 });
+
+  const [blue, setBlue] = useState({ r: 49, g: 187, b: 230 });
 
   function getPink() {
     return "rgb(" + pink.r + "," + pink.g + "," + pink.b + ")";
   }
 
-  function darkenPink() {
+  function getBlue() {
+    return "rgb(" + blue.r + "," + blue.g + "," + blue.b + ")";
+  }
+
+  function darkenBlue() 
+  {
+    const { r, g, b } = blue;
+    setBlue({ r: r-5, g: g - 15, b: b });
+  }
+
+  function darkenPink() 
+  {
     const { r, g, b } = pink;
     setPink({ r: r, g: g - 37, b: b - 17 });
   }
@@ -16,6 +30,7 @@ function App() {
   return (
     <>
       <div className="square">
+        
         <div
           className="top-left-circle"
           onClick={darkenPink}
@@ -23,6 +38,23 @@ function App() {
             backgroundColor: getPink(),
           }}
         ></div>
+
+        <div
+          className="top-right-circle"
+          onClick={darkenBlue}
+          style={{
+            backgroundColor: getBlue(),
+            
+          }}
+        >
+{/* contents of semi circle go here */}
+        
+        
+        </div>
+
+
+
+
       </div>
     </>
   );
